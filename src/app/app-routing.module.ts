6f09d30cb194a6bfea59e13fc697635afab7bc48
shereeja <shereejama90488@gmail.com>
2023-03-06 13:30:25 +0530

    upto path

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { PackagesComponent } from './packages/packages.component';
+import { StayComponent } from './stay/stay.component';
+
+
+
+const routes: Routes = [
+
+  {path : "", component : StayComponent},
+
+  {path : "stay", component : StayComponent},
+
+  {path : "packages", component : PackagesComponent},
+
+  {path : "contact-form" , component :  ContactFormComponent },
+
+ 
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PackagesComponent } from './packages/packages.component';
+import { PackagedetailComponent } from './packagedetail/packagedetail.component';
+import { BookingdetailComponent } from './bookingdetail/bookingdetail.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OfferComponent } from './offer/offer.component';
+import { AboutComponent } from './about/about.component';
+import { CarousalComponent } from './carousal/carousal.component';
+import { FooterComponent } from './footer/footer.component';
+import { StayComponent } from './stay/stay.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PackagesComponent,
+    PackagedetailComponent,
+    BookingdetailComponent,
+    ContactFormComponent,
+    NavbarComponent,
+    OfferComponent,
+    AboutComponent,
+    CarousalComponent,
+    FooterComponent,
+    StayComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
